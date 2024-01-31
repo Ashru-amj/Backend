1cf379570f663e5f3867a130c26ddb7ab2a17462
jenaamj <jenaamj@gmail.com>
2024-02-01 03:12:24 +0530

    dsw

--- db/db.js
+++ db/db.js
@@ -12,11 +12,11 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
   try {
-    console.log(process.env.MONGODB_URL)
+   
     const conection = await mongoose.connect(process.env.MONGODB_URL);
     console.log("Database connected successfully");
   } catch (error) {
-    console.log("Database not connected");
+
     console.log(error)
     
   }
